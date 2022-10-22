1bee27e41e41afb6c793b4bf9f528282b2c4fc21
PranayJaju <98613449+PranayJaju@users.noreply.github.com>
2022-10-22 10:28:02 +0530

    Update decoder3to8.v

--- Lab5/Q1/decoder3to8.v
+++ Lab5/Q1/decoder3to8.v
@@ -2,24 +2,24 @@
 
 module decoder3to8(W, Y);
 input [2:0]W;
-output [7:0]Y;
+output [0:7]Y;
 
-decoder2to4 s1(W[1:0], ~W[2], Y[3:0]);
-decoder2to4 s2(W[1:0], W[2], Y[7:4]);
+decoder2to4 s1(W[1:0], ~W[2], Y[0:3]);
+decoder2to4 s2(W[1:0], W[2], Y[4:7]);
 
 endmodule
 
 module decoder2to4(A, En, D);
     input [1:0]A;
     input En;
-    output [3:0]D;
-    reg [3:0]D;
+    output [0:3]D;
+    reg [0:3]D;
     always@(A or En)
     begin
         if(En==0)D=0;
-        else if(A==0)D=1;
-        else if (A==1)D=2;
-        else if(A==2)D=4;
-        else D=8;
+        else if(A==0)D=8;
+        else if (A==1)D=4;
+        else if(A==2)D=2;
+        else D=1;
     end
 endmodule    
